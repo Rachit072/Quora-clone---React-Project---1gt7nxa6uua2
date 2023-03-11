0290df85d3a180791ee37faf5c6494909b3821c5
Rachit <rachitshrm96@gmail.com>
2023-03-12 01:43:49 +0530

    Added Landing Page

---
+++ src/components/Landing_Page.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Landing_page(){
+
+    return <div>
+        <div className="main-container">
+            <div className="container">
+                <h1 style={{color:"rgb(150,0,0)"}}>Quora</h1>
+                <p>A place to share knowledge and better understand the world</p>
+                <button>Enter</button>
+            </div>
+        </div>
+    </div>
+
+}
+export default Landing_page;
